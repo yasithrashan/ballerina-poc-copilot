f135ba24a1df87f3e04f5997a24ab03f528d9f8e
Yasith Rashan <y.rashan22@gmail.com>
2025-08-22 14:04:45 +0530

    feat: add ai types

--- src/ai/code/code.ts
+++ src/ai/code/code.ts
@@ -1,10 +1,10 @@
-import { generateText } from "ai";
+import { generateText, tool } from "ai";
 import { ANTHROPIC_SONNET_3_5, getAnthropicClinet } from "../connection";
 import { anthropic } from "@ai-sdk/anthropic";
 
 const { text } = await generateText({
     model: anthropic(getAnthropicClinet(ANTHROPIC_SONNET_3_5)),
-    prompt: 'Do you know about ballerina lang'
-})
+    prompt: "Do you know about ballerina lang",
+});
 
 console.log(text);
